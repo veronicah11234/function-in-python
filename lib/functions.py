7e660b7bd36ed8d76fc76224fbeff57ffc2a037e
miriam <miriamwangui525@gmail.com>
2024-02-28 14:37:55 +0300

    using funtionsin python

---
+++ lib/functions.py
@@ -0,0 +1,26 @@
+def greet_programmer():
+    print("Hello, programmer!")
+greet_programmer()
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet(("miriam"))
+
+
+def greet_with_default(name = "Mary"):
+        print(f"Hello, {name}!")
+
+greet_with_default()
+
+
+def add(num1 , num2):
+     return num1 + num2
+print(add(34 , 45))
+
+def halve(num):
+     return num / 2
+print(halve(4))
+     
+     
